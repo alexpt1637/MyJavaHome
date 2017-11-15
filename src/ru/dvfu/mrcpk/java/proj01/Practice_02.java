8f47f1a9e3dadce7ef740089ca2277d439007622
alexpt1637 <alexpt1637@yandex.ru>
2017-11-15 12:20:31 +1000

    e2 - степень числа 10, на которую следует умножить данное число

--- src/ru/dvfu/mrcpk/java/proj01/Practice_02.java
+++ src/ru/dvfu/mrcpk/java/proj01/Practice_02.java
@@ -7,7 +7,7 @@ public class Practice_02 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите числовое значение переменной и нажмите Enter: ");
+        /*System.out.println("Введите числовое значение переменной и нажмите Enter: ");
         int count = in.nextInt();
 
         if (count > 5){
@@ -19,13 +19,14 @@ public class Practice_02 {
         }else{
             count = count + 5;
             System.out.println("Значение переменной < 5 и увеличено на 5: " + count);
-        }
+        }*/
 
 
         /* count = count > 15?count + 1:count + 5;  // Сокращенная запись условного оператора
         System.out.println("Результат выполнения сокращенной записи оператора: " + count);
         */
 
-
+        double d = 2e2;         // e2 - степень числа 10, на которую следует умножить данное число
+        System.out.println(d);
     }
 }
