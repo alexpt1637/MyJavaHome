8df9c3c8f6b559c8c6fbb440866760d12c8912db
alexpt1637 <alexpt1637@yandex.ru>
2017-11-20 16:39:41 +1000

    Вызван второй унаследованный от родительского класса метод

--- src/ru/dvfu/mrcpk/java/proj01/MyClassUser/Main.java
+++ src/ru/dvfu/mrcpk/java/proj01/MyClassUser/Main.java
@@ -3,8 +3,9 @@ package ru.dvfu.mrcpk.java.proj01.MyClassUser;
 public class Main {
     public static void main(String[] args) {
 
-        MyUserProject myUserProject = new MyUserProject();  // Создан экземпляр дочернего класса
+        MyUserProject myUserProject1 = new MyUserProject();  // Создан экземпляр дочернего класса
 
-        myUserProject.времяНачалаТеста();                   // Вызван унаследованный от родительского класса метод
+        myUserProject1.имяПользователя();                   // Вызван унаследованный от родительского класса метод
+        myUserProject1.времяНачалаТеста();                   // Вызван унаследованный от родительского класса метод
     }
 }
--- src/ru/dvfu/mrcpk/java/proj01/MyClassUser/SuperUser.java
+++ src/ru/dvfu/mrcpk/java/proj01/MyClassUser/SuperUser.java
@@ -3,21 +3,29 @@ package ru.dvfu.mrcpk.java.proj01.MyClassUser;
 import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class SuperUser extends Main {
 
-    public SuperUser() {                            // Создадим родительский класс
+    public SuperUser() {                            // Создан родительский класс
         String Имя;                                 // Поля (свойства) родительского класса
         int НомерПользователя;
         Data ВремяНачалаТеста;
     }
 
-    public void времяНачалаТеста(){                 // Создадим метод, определяющий время начала тестирования
+    public void имяПользователя(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя пользователя и нажмите Enter: ");
+        String Имя = in.nextLine();
+        System.out.println("Имя пользователя: " + Имя);
+    }
+
+    public void времяНачалаТеста(){                 // Создан метод, определяющий время начала тестирования
         long curDate = System.currentTimeMillis();
         Date date = new Date(curDate);
         SimpleDateFormat sdf = new SimpleDateFormat();
 
-        // Передаем новый шаблон
+        // Передаем новый шаблон даты/времени
         sdf.applyPattern("dd-MM-yyyy HH:mm");
         System.out.println("Время начала теста: " + sdf.format(date));       // dd-MM-yyyy HH:mm 08-11-2017 15:21
     }
