665261112596b8be3b457786d184ee387220f075
alexpt1637 <alexpt1637@yandex.ru>
2017-11-23 00:09:49 +1000

    Временный метод, имитирующий выполнение программы

--- src/ru/dvfu/mrcpk/java/proj01/MyClassUser/MyUserProject.java
+++ src/ru/dvfu/mrcpk/java/proj01/MyClassUser/MyUserProject.java
@@ -1,4 +1,16 @@
 package ru.dvfu.mrcpk.java.proj01.MyClassUser;
 
+import java.util.Scanner;
+
 public class MyUserProject extends SuperUser {  // Создан дочерний класс от родительского
+
+    // Временный метод, имитирующий выполнение программы тестирования
+    public void имитацияТеста2(){
+        SuperUser superUser1 = new SuperUser();
+        // SuperUser superUser2 = new SuperUser();
+
+        superUser1.имитацияТеста();
+        // superUser2.имитацияТеста();
+    }
+
 }
--- src/ru/dvfu/mrcpk/java/proj01/MyClassUser/SuperUser.java
+++ src/ru/dvfu/mrcpk/java/proj01/MyClassUser/SuperUser.java
@@ -1,5 +1,7 @@
 package ru.dvfu.mrcpk.java.proj01.MyClassUser;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,25 +12,49 @@ public class SuperUser extends Main {
     public SuperUser() {                            // Создан родительский класс
         String Имя;                                 // Поля (свойства) родительского класса
         int НомерПользователя;
-        Data ВремяНачалаТеста;
+        Date ВремяНачалаТеста, date1, date2, timeTest, time1, time2;
+
+
     }
 
-    public void имяПользователя(){
+    public void имяПользователя(){                  // Метод, определяющий имя пользователя
         Scanner in = new Scanner(System.in);
         System.out.println("Введите имя пользователя и нажмите Enter: ");
         String Имя = in.nextLine();
         System.out.println("Имя пользователя: " + Имя);
+        // --> передача информации в класс или метод, определяющий результаты тестирования
     }
 
-    public void времяНачалаТеста(){                 // Создан метод, определяющий время начала тестирования
+    public void времяНачалаТеста(){                 // Метод, определяющий время начала тестирования
         long curDate = System.currentTimeMillis();
-        Date date = new Date(curDate);
+        Date date1 = new Date(curDate);
         SimpleDateFormat sdf = new SimpleDateFormat();
 
         // Передаем новый шаблон даты/времени
         sdf.applyPattern("dd-MM-yyyy HH:mm");
-        System.out.println("Время начала теста: " + sdf.format(date));       // dd-MM-yyyy HH:mm 08-11-2017 15:21
+        System.out.println("Время начала теста: " + sdf.format(date1));       // dd-MM-yyyy HH:mm 08-11-2017 15:21
     }
 
+    public void времяОкончанияТеста(){                 // Метод, определяющий время окончания тестирования
+        long curDate = System.currentTimeMillis();
+        Date date2 = new Date(curDate);
+        SimpleDateFormat sdf = new SimpleDateFormat();
+
+        // Передаем новый шаблон даты/времени
+        sdf.applyPattern("dd-MM-yyyy HH:mm");
+        System.out.println("Время окончания теста: " + sdf.format(date2));       // dd-MM-yyyy HH:mm 08-11-2017 15:21
+    }
+
+    // Временный метод, имитирующий выполнение программы тестирования
+    public void имитацияТеста(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Для имитации выполнения программы введите целое число и нажмите Enter: ");
+        int i = in.nextInt();
+        System.out.println("Для имитации выполнения программы Вы ввели целое число: " + i);
+    }
+
+    public void времяТеста(){
+
+    }
 
 }
