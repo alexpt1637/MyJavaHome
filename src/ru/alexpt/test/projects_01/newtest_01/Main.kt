8a9490290eb4ae7e021b5d54058879fde2416eb4
AlexPt <alexpt1637@yandex.ru>
2018-05-29 23:04:41 +1000

    Тестирование объектов Movie

--- src/ru/alexpt/test/projects_01/newtest_01/Main.kt
+++ src/ru/alexpt/test/projects_01/newtest_01/Main.kt
@@ -4,11 +4,11 @@ import ru.alexpt.test.kotlin.exc
 import ru.alexpt.test.kotlin.fortest
 
 fun main(args: Array<String>) {
-//    phraseGenerator()           // Генератор фраз
+    phraseGenerator()           // Генератор фраз
 //    startGame()                 // Игра в угадывание
 //    myDog()                     // Интересуемся у щенка, кто он...
 //    myDogRandom()               // Интересуемся у щенка, кто он (random)...
 //    fortest()                   // Перемножение множителей из указанного диапазона чисел
 //    exc()                       // Обработка исключения (деление на ноль)
-    onMovie()                   // Тестирование объектов Movie
+//    onMovie()                   // Тестирование объектов Movie
 }
