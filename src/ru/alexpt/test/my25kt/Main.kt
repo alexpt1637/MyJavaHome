35668e4fe90598ee4dae84eb011cb719da25b2be
AlexPt <alexpt1637@yandex.ru>
2018-06-09 00:40:37 +1000

    SimpleGUI.kt - Main.kt - frame2.isVisible

--- src/ru/alexpt/test/my25kt/Main.kt
+++ src/ru/alexpt/test/my25kt/Main.kt
@@ -3,4 +3,8 @@ package ru.alexpt.test.my25kt
 fun main(args: Array<String>) {
     val app = SimpleGUI()
     app.isVisible = true
+
+    val frame2 = MyFrame(25,25)
+    frame2.title = "Heart"
+    frame2.isVisible = true
 }
