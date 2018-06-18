a2005659e6e408ce8e30eccca018df49ee1d6a38
AlexPt <alexpt1637@yandex.ru>
2018-06-18 20:58:36 +1000

    приложение JavaFX

---
+++ src/ru/alexpt/test/javaFX/Main.java
@@ -0,0 +1,45 @@
+package ru.alexpt.test.javaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+
+    // в методе main, чтобы запустить само приложение JavaFX, представленное классом Application, вызывается метод launch():
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {   // передается объект Stage, который представляет пользовательский интерфейс
+        // На десктопах Stage будет представлять графическое окно
+        // При запуске приложения среда JavaFX будет вызывать данный метод (start) и передавать в него объект Stage
+
+        // установка надписи
+        Text text = new Text("Hello from JavaFX!");
+        // мы используем визуальный компонент javafx.scene.text.Text, который представляет простую текстовую надпись
+        // При создании элемента Text мы можем передать ему выводимый текст, а также настроить его положение с помощью его методов:
+        text.setLayoutY(80);    // установка положения надписи по оси Y
+        text.setLayoutX(100);   // установка положения надписи по оси X
+
+        // Все визуальные элементы, которые мы хотим отобразить в Stage, помещаются в объект javafx.scene.Scene или на сцену.
+        // Scene - это контейнер верхнего уровня для всех графических элементов. Однако напрямую в Scene объект Text мы поместить не можем.
+        // Класс Scene предусматривает установку корневого элемента или контейнера, который содержит все остальные элементы.
+
+        // Таким образом, вначале элемент Text помещается в элемент Group, который представляет контейнер для группы элементов.
+        // Затем элемент Group устанавливается в качестве корневого элемента Scene. И в конце элемент Scene устанавливается для объекта Stage.
+        Group group = new Group(text);      // создание и установка группы элементов
+        Scene scene = new Scene(group);     // создание сцены
+        stage.setScene(scene);              // установка сцены для объекта Stage
+
+        // В конце метода start мы можем настроить объект Stage, например, задать заголовок окна, а также его размеры:
+        stage.setTitle("First Application");// установка заголовка окна
+        stage.setWidth(300);                // установка ширины окна
+        stage.setHeight(250);               // установка длины окна
+        stage.show();                       // отображаем окно на экране устройства
+        // С помощью метода show объект Stage отображается на экране устройства
+    }
+}
---
+++ src/ru/alexpt/test/javaFX/Test.kt
@@ -0,0 +1,11 @@
+package ru.alexpt.test.javaFX
+
+fun main(args: Array<String>) {
+    val t = test()
+    println(t)
+}
+
+fun test(): String{
+    val str = "Hello Kotlin!"
+    return str
+}
